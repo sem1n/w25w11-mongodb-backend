f42a8492b015dbf948f5a1e2c436723d3d96f1a2
sem1n <songsm1129@gmail.com>
2025-11-17 19:10:12 +0900

    👔MongoDB 접근

--- src/main/kotlin/kr/ac/kumoh/s20240624/w25w11_mongodb_backend/controller/SongController.kt
+++ src/main/kotlin/kr/ac/kumoh/s20240624/w25w11_mongodb_backend/controller/SongController.kt
@@ -2,7 +2,12 @@ package kr.ac.kumoh.s20240624.w25w11_mongodb_backend.controller
 
 import kr.ac.kumoh.s20240624.w25w11_mongodb_backend.model.Song
 import kr.ac.kumoh.s20240624.w25w11_mongodb_backend.service.SongService
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -11,6 +16,33 @@ import org.springframework.web.bind.annotation.RestController
 class SongController(
     private val service: SongService
 ) {
+    // Create
+    @PostMapping
+    fun addSong(@RequestBody song: Song): Song = service.addSong(song)
+
+    // Read (Retrieve)
     @GetMapping
     fun getAllSongs(): List<Song> = service.getAllSongs()
+
+    @GetMapping("/{id}")
+    fun getSongById(@PathVariable id: String): Song? = service.getSongById(id)
+
+    @GetMapping("/singer/{singer}")
+    fun getSongBySinger(@PathVariable singer: String): List<Song> = service.getSongBySinger(singer)
+
+    // Update
+    @PutMapping("/{id}")
+    fun updateSong(
+        @PathVariable id: String,
+        @RequestBody songDetails: Song
+    ): Song? =  service.updateSong(id, songDetails)
+
+    // Delete
+    @DeleteMapping("/{id}")
+    fun deleteSong(@PathVariable id: String): Map<String, String> {
+        return if (service.deleteSong(id))
+            mapOf("status" to "deleted")
+        else
+            mapOf("status" to "not found")
+    }
 }
