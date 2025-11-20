ab9d724cce5a71a2988a4e46a174c4105147cd8f
sem1n <songsm1129@gmail.com>
2025-11-20 14:21:00 +0900

    🩺CORS 에러 해결

--- src/main/kotlin/kr/ac/kumoh/s20240624/w25w11_mongodb_backend/controller/SongController.kt
+++ src/main/kotlin/kr/ac/kumoh/s20240624/w25w11_mongodb_backend/controller/SongController.kt
@@ -2,6 +2,7 @@ package kr.ac.kumoh.s20240624.w25w11_mongodb_backend.controller
 
 import kr.ac.kumoh.s20240624.w25w11_mongodb_backend.model.Song
 import kr.ac.kumoh.s20240624.w25w11_mongodb_backend.service.SongService
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -13,36 +14,13 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/songs")
+@CrossOrigin(origins = ["http://localhost:5173"])
 class SongController(
     private val service: SongService
 ) {
-    // Create
-    @PostMapping
-    fun addSong(@RequestBody song: Song): Song = service.addSong(song)
-
-    // Read (Retrieve)
     @GetMapping
     fun getAllSongs(): List<Song> = service.getAllSongs()
 
     @GetMapping("/{id}")
     fun getSongById(@PathVariable id: String): Song? = service.getSongById(id)
-
-    @GetMapping("/singer/{singer}")
-    fun getSongBySinger(@PathVariable singer: String): List<Song> = service.getSongBySinger(singer)
-
-    // Update
-    @PutMapping("/{id}")
-    fun updateSong(
-        @PathVariable id: String,
-        @RequestBody songDetails: Song
-    ): Song? =  service.updateSong(id, songDetails)
-
-    // Delete
-    @DeleteMapping("/{id}")
-    fun deleteSong(@PathVariable id: String): Map<String, String> {
-        return if (service.deleteSong(id))
-            mapOf("status" to "deleted")
-        else
-            mapOf("status" to "not found")
-    }
 }
