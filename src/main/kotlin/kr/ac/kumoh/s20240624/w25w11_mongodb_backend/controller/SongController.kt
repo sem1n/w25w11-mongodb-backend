9af0e1e1854c5b7ed918b35a10ba2b9e65f18e89
sem1n <songsm1129@gmail.com>
2025-11-24 15:50:24 +0900

    🚀Backend 완성

--- src/main/kotlin/kr/ac/kumoh/s20240624/w25w11_mongodb_backend/controller/SongController.kt
+++ src/main/kotlin/kr/ac/kumoh/s20240624/w25w11_mongodb_backend/controller/SongController.kt
@@ -2,6 +2,7 @@ package kr.ac.kumoh.s20240624.w25w11_mongodb_backend.controller
 
 import kr.ac.kumoh.s20240624.w25w11_mongodb_backend.model.Song
 import kr.ac.kumoh.s20240624.w25w11_mongodb_backend.service.SongService
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
@@ -10,7 +11,9 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import java.net.URI
 
 @RestController
 @RequestMapping("/api/songs")
@@ -18,9 +21,86 @@ import org.springframework.web.bind.annotation.RestController
 class SongController(
     private val service: SongService
 ) {
+    // Create
+    @PostMapping
+    fun addSong(
+        @RequestBody song: Song
+    ): ResponseEntity<Song> {
+        val createdSong = service.addSong(song)
+        // 201 Created
+        // 생성된 리소스의 URI 반환
+        return ResponseEntity
+            .created(URI("/api/songs/${createdSong.id}"))
+            .body(createdSong)
+    }
+
+    // Read (Retrieve)
     @GetMapping
-    fun getAllSongs(): List<Song> = service.getAllSongs()
+    fun getAllSongs(): ResponseEntity<List<Song>> {
+        val songs = service.getAllSongs()
+        if (songs.isEmpty()) {
+            // 204 No Content
+            return ResponseEntity.noContent().build()
+        }
+
+        // 200 OK
+        return ResponseEntity.ok(songs)
+    }
 
     @GetMapping("/{id}")
-    fun getSongById(@PathVariable id: String): Song? = service.getSongById(id)
+    fun getSongById(
+        @PathVariable id: String
+    ): ResponseEntity<Song> {
+        val song = service.getSongById(id)
+
+        // 있으면 200 OK, 없으면 404 Not Found
+        return song?.let {
+            ResponseEntity.ok(it)
+        } ?: ResponseEntity.notFound().build()
+    }
+
+    @GetMapping("/search")
+    fun getSongBySinger(
+        // 변수 이름이 parameter 문자열이 됨
+        // /api/songs/search?singer=HUNTR/X
+        @RequestParam singer: String
+    ): ResponseEntity<List<Song>> {
+        val songs = service.getSongBySinger(singer)
+
+        if (songs.isEmpty()) {
+            // 204 No Content
+            return ResponseEntity.noContent().build()
+        }
+
+        // 200 OK
+        return ResponseEntity.ok(songs)
+    }
+
+    // Update
+    @PutMapping("/{id}")
+    fun updateSong(
+        @PathVariable id: String,
+        @RequestBody songDetails: Song
+    ): ResponseEntity<Song> {
+        val updatedSong = service.updateSong(id, songDetails)
+
+        // 성공시 200 OK와 업데이트된 객체 반환, 실패시 404 Not Found
+        return updatedSong?.let {
+            ResponseEntity.ok(it)
+        } ?: ResponseEntity.notFound().build()
+    }
+
+    // Delete
+    @DeleteMapping("/{id}")
+    fun deleteSong(
+        @PathVariable id: String
+    ): ResponseEntity<Void> {
+        return if (service.deleteSong(id)) {
+            // 204 No Content
+            ResponseEntity.noContent().build()
+        } else {
+            // 404 Not Found
+            ResponseEntity.notFound().build()
+        }
+    }
 }
